7c2d1e6ee1e1e1a2af69408c336f11c5ceefed7e
Santiago Echeverry <santiagocherrys@hotmail.com>
2024-04-08 09:54:31 -0500

    Se cambia urlCompanies con otro link de sevicio, asi no deberia proner problema el json server

--- js/libs/API/api.js
+++ js/libs/API/api.js
@@ -1,5 +1,5 @@
 /* URL para acceder al json server */
 
-export const urlCompanies = 'https://json-server-filtro-javascript.onrender.com/companies';
+export const urlCompanies = 'https://json-server-filtro-javascript2.onrender.com/companies';
 export const urlJobs = 'https://json-server-filtro-javascript.onrender.com/jobs';
 
